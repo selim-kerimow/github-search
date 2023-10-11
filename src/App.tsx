24c2a44bb8ae880260559f075202db143219eb03
Selim <selimkerimow68@gmail.com>
2023-10-11 21:16:51 +0500

    created routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { Routes, Route} from 'react-router-dom'
+
+// pages
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 
-function App() {
+
+const App: React.FC = () => {
+
+
 
   return (
-    <>
+    <div className="">
 
-    </>
-  )
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Favorites: React.FC = () => {
+
+
+
+    return (
+        <div>
+            Favorites
+        </div>
+    )
+}
+
+export default Favorites
