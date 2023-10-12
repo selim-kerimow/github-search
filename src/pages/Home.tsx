66ea2babfbe55321008e6138151e1680f3a89aa5
Selim <selimkerimow68@gmail.com>
2023-10-12 18:45:14 +0500

    created homepage layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,30 @@
 import React from "react";
 
+// css 
+import '../css/home.css'
+
 
 const Home: React.FC = () => {
 
-
-
     return (
-        <div>
-            Home
+        <div className="text-center pt-10">
+
+        <div className="w-[30%] mx-auto relative max-sm:w-[90%] max-md:w-[70%] max-lg:w-[50%]">
+            <input 
+                placeholder="Search for users" 
+                className="home--input" />
+
+            <ul className="absolute w-full mx-auto rounded-md max-h-[200px] overflow-y-scroll">
+                    <li 
+                        className="bg-slate-300 px-4 py-2 hover:bg-teal-600 hover:text-white cursor-pointer" 
+                        >
+                        Repo
+                    </li>
+            </ul>
+
+        </div>
+
+            
         </div>
     )
 }
