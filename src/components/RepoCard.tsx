792e5f9d9f8b3a731b19d8e5f1448df40024baec
Selim <selimkerimow68@gmail.com>
2023-10-13 16:15:30 +0500

    finished

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { IRepo } from "../models/reposModel";
+import { useActions, useAppSelector } from "../hooks/reduxHooks";
 
 // react-icons
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
@@ -10,6 +11,25 @@ interface RepoCardProps {
 }
 
 const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
+
+    // Redux Hooks
+    const {addFavorites, removeFavorites} = useActions()
+    const { favorites } = useAppSelector(state => state.githubSlice)
+
+    // states 
+    const [isFav, setIsFav] = useState<boolean>(favorites.includes(repo.html_url))
+    
+    // functions
+    const addToFavorites = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setIsFav(true)
+        addFavorites(repo.html_url)
+    }
+    const removeFromFavorites = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setIsFav(false)
+        removeFavorites(repo.html_url)
+    }
 
     return (
         <div className="w-full border rounded px-3 py-5 my-5 hover:bg-teal-600 hover:text-white cursor-pointer max-extraSmall:text-sm">
@@ -22,11 +42,12 @@ const RepoCard: React.FC<RepoCardProps> = ({ repo }) => {
                 {repo.description && <p className="mb-4"><strong>Descriptiion: </strong>{repo.description}</p>}
 
                 <div className="flex justify-between px-5 max-sm:p-0">
-                    
-                        <button className="cursor-poiner"> <AiFillHeart size={25} color="red"/> </button>
-                        
-                        <button className="cursor-poiner"> <AiOutlineHeart size={25}/> </button>
-                    
+                    { isFav 
+                        ? 
+                        <button onClick={removeFromFavorites} className="cursor-poiner"> <AiFillHeart size={25} color="red"/> </button>
+                        : 
+                        <button onClick={addToFavorites} className="cursor-poiner"> <AiOutlineHeart size={25}/> </button>
+                    }
                     <a 
                         className="bg-slate-600 rounded-md p-2 text-white cursor-pointer max-extraSmall:w-[70%] hover:bg-yellow-300 hover:text-black" 
                         href={repo.html_url}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,33 +38,31 @@ const Home: React.FC = () => {
         <div className="text-center pt-10 h-[100%] min-h-[calc(100vh-59px)]">
             { isError && <h3 className="text-lg text-red-500 my-4">Something went wrong! Make sure you are connected to internet!</h3>}
 
-        <div className="w-[30%] mx-auto relative max-sm:w-[90%] max-md:w-[70%] max-lg:w-[50%]">
-            <input 
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder="Search for users" 
-                className="home--input" />
+            <div className="w-[30%] mx-auto relative max-sm:w-[90%] max-md:w-[70%] max-lg:w-[50%]">
+                <input 
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search for users" 
+                    className="home--input" />
 
-            {dropdown && <ul className="absolute w-full mx-auto rounded-md max-h-[200px] overflow-y-scroll">
-                { isLoading && <p>Loading...</p>}
-                { users && users.map(user => 
-                    <li 
-                        className="bg-slate-300 px-4 py-2 hover:bg-teal-600 hover:text-white cursor-pointer" 
-                        key={user.id}
-                        onClick={() => listClickHandler(user.login)}
-                        >
-                        { user.login }
-                    </li>
-                    )}
-            </ul>}
+                {dropdown && <ul className="absolute w-full mx-auto rounded-md max-h-[200px] overflow-y-scroll">
+                    { isLoading && <p>Loading...</p>}
+                    { users && users.map(user => 
+                        <li 
+                            className="bg-slate-300 px-4 py-2 hover:bg-teal-600 hover:text-white cursor-pointer" 
+                            key={user.id}
+                            onClick={() => listClickHandler(user.login)}
+                            >
+                            { user.login }
+                        </li>
+                        )}
+                </ul>}
 
-            <div>
-                { ReposIsLoading && <h1>Loading...</h1>}
-                { repos?.map(repo => <RepoCard repo={repo} key={repo.id}/>)}
-            </div>  
-        </div>
-
-            
+                <div>
+                    { ReposIsLoading && <h1>Loading...</h1>}
+                    { repos?.map(repo => <RepoCard repo={repo} key={repo.id}/>)}
+                </div>  
+            </div>
         </div>
     )
 }
