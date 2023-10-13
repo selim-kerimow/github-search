b931b4ced9431da4bcee2dfd610ee1ef5a0009d3
Selim <selimkerimow68@gmail.com>
2023-10-13 05:49:37 +0500

    connected Favorites component with redux-toolkit

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useAppSelector } from "../hooks/reduxHooks";
 
 
 const Favorites: React.FC = () => {
 
+    const { favorites } = useAppSelector(state => state.githubSlice)
+
     return (
-        <div className="h-[100%] min-h-[calc(100vh-60px)] text-center pt-10 mx-auto max-sm:w-[90%]">
+        <div className="text-center pt-10 mx-auto max-sm:w-[90%]">
             <h1 className="text-bold text-4xl mb-6">Favorites URL</h1>
             <ul>
-                <li className="mb-4 hover:text-cyan-500 hover:underline">
-                    <a target="_blank">
-                    </a>
-                </li>
+                { favorites?.map(fav => 
+                    <li className="mb-4 hover:text-cyan-500 hover:underline">
+                        <a href={fav} target="_blank">
+                            {fav}
+                        </a>
+                    </li>
+                )}
             </ul>
         </div>
     )
